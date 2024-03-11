d1eea64e0fc0e462055d93c4d69c2b6d90283563
Сергей <crackkc@mail.ru>
2024-03-11 15:01:34 +0300

    convert nan to null

--- superset/datasource/api.py
+++ superset/datasource/api.py
@@ -16,6 +16,8 @@
 # under the License.
 import logging
 
+import numpy
+import numpy as np
 from flask_appbuilder.api import expose, protect, safe
 
 from superset import app, event_logger
@@ -122,7 +124,9 @@ class DatasourceRestApi(BaseSupersetApi):
                 limit=row_limit,
                 denormalize_column=denormalize_column,
             )
-            return self.response(200, result=payload)
+            payload = np.array(payload)
+            payload = np.where(np.isnan(payload), None, payload)
+            return self.response(200, result=payload.tolist())
         except KeyError:
             return self.response(
                 400, message=f"Column name {column_name} does not exist"
